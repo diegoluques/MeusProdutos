d4995cf3210ae7929d7737b9f1035ede80665c9b
Diego Luques <luquesdiego@outlook.com>
2023-01-28 12:25:40 -0400

    Inserido FluentAPI e EF

---
+++ src/DevIO.Infra/Data/Mappings/FornecedoreConfig.cs
@@ -0,0 +1,34 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class FornecedoreConfig : EntityTypeConfiguration<Fornecedor>
+    {
+        public FornecedoreConfig()
+        {
+            HasKey(f => f.Id);
+            Property(f => f.Nome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(f => f.Documento)
+                .IsRequired()
+                .HasMaxLength(14)
+                .HasColumnAnnotation("IX_Documento",
+                    new IndexAnnotation(new IndexAttribute { IsUnique = true }));
+
+            HasRequired(f => f.Endereco)
+                .WithRequiredPrincipal(e => e.Fornecedor);
+
+            ToTable("Fornecedores");
+        }
+    }
+}
---
+++ src/DevIO.Infra/Data/Migrations/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace DevIO.Infra.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DevIO.Infra.Data.Context.MeuDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+    }
+}
