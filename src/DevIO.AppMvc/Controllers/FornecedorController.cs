1c502a7175d0bc001d0b97aa707c4b539bf7097f
Diego Luques <luquesdiego@outlook.com>
2023-01-30 22:46:51 -0400

    Testando o funcionamento do serviço

---
+++ src/DevIO.AppMvc/Controllers/FornecedorController.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.Business.Models.Fornecedores.Services;
+using DevIO.Infra.Data.Repository;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly IFornecedorService _fornecedorService;
+
+        public FornecedorController()
+        {
+            _fornecedorService = new FornecedorService(new FornecedorRepository(),
+                                                       new EnderecoRepository());
+        }
+
+        // GET: Fornecedor
+        public async Task<ActionResult> Index()
+        {
+            var fornecedor = new Fornecedor
+            {
+                Nome = "",
+                Endereco = new Endereco
+                {
+
+                },
+            };
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return new EmptyResult();
+        }
+    }
+}
--- src/DevIO.Infra/Data/Repository/EnderecoRepository.cs
+++ src/DevIO.Infra/Data/Repository/EnderecoRepository.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace DevIO.Infra.Data.Repository
 {
-    class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
     {
         public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
         {
